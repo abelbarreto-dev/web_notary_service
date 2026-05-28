ad2b5ecb4f0733c27b591e0efde69fbb81213d7f
abelbarreto-dev <abelbcarvalho@gmail.com>
2026-05-28 20:27:43 -0300

    infra: resolvers for each route created

--- backend/src/graphql/resolvers/notary.resolvers.ts
+++ backend/src/graphql/resolvers/notary.resolvers.ts
@@ -1,11 +1,39 @@
+import { PrismaContext } from "@infra/prisma.context";
+import { NotaryId, NotaryInput } from "@infra/notary.models";
+import { NotaryController } from "../../controllers/notary.controller";
+
 export const notaryResolvers = {
     Query: {
-        findAllNotaries: async () => {},
+        findAllNotaries: async (
+            _: any,
+            notary: any,
+            context: PrismaContext,
+        ) => {
+            const controller = new NotaryController(context.prisma);
+        },
     },
 
     Mutation: {
-        createNotary: async () => {},
-        updateNotary: async () => {},
-        deleteNotary: async () => {},
+        createNotary: async (
+            _: any,
+            notary: NotaryInput,
+            context: PrismaContext,
+        ) => {
+            const controller = new NotaryController(context.prisma);
+        },
+        updateNotary: async (
+            _: any,
+            notary: NotaryInput,
+            context: PrismaContext,
+        ) => {
+            const controller = new NotaryController(context.prisma);
+        },
+        deleteNotary: async (
+            _: any,
+            notary: NotaryId,
+            context: PrismaContext,
+        ) => {
+            const controller = new NotaryController(context.prisma);
+        },
     },
 };
--- backend/src/graphql/resolvers/user.resolvers.ts
+++ backend/src/graphql/resolvers/user.resolvers.ts
@@ -1,12 +1,26 @@
+import { PrismaContext } from "@infra/prisma.context";
+import { UserId, UserInput, UserLogin } from "@infra/user.models";
+import { UserController } from "../../controllers/user.controller";
+
 export const userResolvers = {
     Query: {
-        findAllUsers: async () => {},
+        findAllUsers: async (_: any, user: any, context: PrismaContext) => {
+            const controller = new UserController(context.prisma);
+        },
     },
 
     Mutation: {
-        createUser: async () => {},
-        updateUser: async () => {},
-        deleteUser: async () => {},
-        singInUser: async () => {},
+        createUser: async (_: any, user: UserInput, context: PrismaContext) => {
+            const controller = new UserController(context.prisma);
+        },
+        updateUser: async (_: any, user: UserInput, context: PrismaContext) => {
+            const controller = new UserController(context.prisma);
+        },
+        deleteUser: async (_: any, user: UserId, context: PrismaContext) => {
+            const controller = new UserController(context.prisma);
+        },
+        singInUser: async (_: any, user: UserLogin, context: PrismaContext) => {
+            const controller = new UserController(context.prisma);
+        },
     },
 };
